79f0d564bef59a64b5e2f38478f87b784921c6dc
TerokRivaldo <69893181+TerokRivaldo@users.noreply.github.com>
2020-11-09 18:48:31 +0700

    Js

---
+++ Pertemuan23/Exercise_BMI/index.js
@@ -0,0 +1,10 @@
+//Menghitung BMI
+
+let berat = prompt("Masukan Berat : ");
+console.log("Berat anda adalah" + berat);
+
+let tinggi = prompt("Masukan Tinggi : ");
+console.log("Tinggi anda adalah "+ tinggi);
+
+let bmi = berat / (tinggi * tinggi);
+console.log("BMI = " + bmi);
---
+++ Pertemuan23/index.js
@@ -0,0 +1,51 @@
+console.log("Hello JavaScript");
+
+//variable
+
+//let & const
+// let umur = 25;
+// let nama = "Jhon";
+// let nilai = 5.5;
+// let duda = false;
+// console.log(umur);
+// console.log(nama);
+
+// let jlhAnak; //undifine
+// const tahunLahir = 2019;
+// console.log(tahunLahir);
+
+//Oparator 
+//aritmatika
+
+// let a1 = 5;
+// let a2 = 5;
+// let hasil = a1+a2;
+// console.log (hasil);
+
+// let umur = 17;
+// let namaDepan = "Jhon";
+// let namaBelakang = "Do";
+// console.log("Nama Saya " + namaDepan + " " + namaBelakang + umur + "tahun" );
+
+//operator perbandingan
+
+// let angka1 = 5;                 
+// let angka2 = 5;
+// let hasil = angka1 === angka2;
+// console.log(hasil);
+
+//ternary operator
+
+// let angka = 5;
+// let hasil = angka <= 0 ? "Negatif" : "Positif"; 
+// console.log(hasil);
+
+// //operator logika
+// let kondisi1 = true;
+// let kondisi2 = false;
+
+// console.log(kondisi1 && kondisi2);
+// console.log(!kondisi1);
+
+let angka = prompt("Masukan angka : ");
+console.log("Angka yang dimasukan adalah" + angka);
